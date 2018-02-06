f97c40ea054643e28ad75e0ffadc310524ab6f2f
Noah D. Brenowitz <nbren12@gmail.com>
2018-02-05 17:25:27 -0800

    remove old script

--- configure_and_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-$camcfg/build-namelist -namelist "&atm iopfile='/inputdata/atm/cam/scam/iop/bomex.nc'
-nhtfrq=-1   single_column=.true. scmlat=0.0 scmlon=0.0  /" -config /bld/config_cache.xml
